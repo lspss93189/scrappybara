9c9735dc4c22ea4ced54b002687bc0a344ec7361
Eric Perriard <eric@capybaralabs.com>
2020-10-31 14:29:44 +0900

    added form extractor in CLI

--- scrappybara/__main__.py
+++ scrappybara/__main__.py
@@ -3,11 +3,13 @@ if __name__ == "__main__":
     from scrappybara.cli.download import download
     from scrappybara.cli.extract_items import extract_items
     from scrappybara.cli.extract_classes import extract_classes
+    from scrappybara.cli.extract_forms import extract_forms
 
     commands = {
         'download': download,
         'extract_classes': extract_classes,
         'extract_items': extract_items,
+        'extract_forms': extract_forms,
     }
 
     if len(sys.argv) == 1:
